91cdca2cb48175968043d144c70d1462ea6b51ef
schne564 <Schne564@gmail.com>
2025-08-10 19:31:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function formatWithSuffix(value) {
   if (value >= 1e12) return (value / 1e12).toFixed(2) + " T";
   if (value >= 1e9) return (value / 1e9).toFixed(2) + " G";
   if (value >= 1e6) return (value / 1e6).toFixed(2) + " M";
+  if (value >= 1e3) return (value / 1e3).toFixed(2) + " K";
   return value.toLocaleString();
 }
 
@@ -11,10 +12,10 @@ fetch(endpoint)
   .then((res) => res.json())
   .then((data) => {
     document.getElementById("address").textContent = data.address;
-    document.getElementById("workers").textContent = data.workers;
+    document.getElementById("workers").textContent = formatWithSuffix(data.workers);
     document.getElementById("bestshare").textContent = formatWithSuffix(data.bestshare);
-    document.getElementById("difficulty").textContent = data.difficulty.toLocaleString();
-    document.getElementById("lastBlock").textContent = data.lastBlock;
+    document.getElementById("difficulty").textContent = formatWithSuffix(data.difficulty);
+    document.getElementById("lastBlock").textContent = formatWithSuffix(data.lastBlock);
     document.getElementById("chance").textContent = (data.chance * 100).toExponential(2) + " %";
   })
   .catch((err) => {
